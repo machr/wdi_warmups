4271dab14a3adb55c9b4f63a55d1e5e938093af2
Mark Powell <hi@powellmark.com>
2017-02-13 10:22:06 +1100

    added functon for getting scores

---
+++ wk02/minigolf-scorecard/scorecard.js
@@ -0,0 +1,47 @@
+var bob = [3, 2, 6, 11, 9, 2, 6, 9, 10];
+var jimbo = [ 5,12,9,22,13,7,16,10,11 ];
+var fish = [ 2,2,4,5,4,3,6,4,1,];
+
+var totalScores = function(scores){
+  var score = 0;
+  for (var i = 0; i < scores.length; i++) {
+    score += scores[i];
+  }
+  return score;
+};
+
+var golfers = {
+  bob: {
+    strokes:[3, 2, 6, 11, 9, 2, 6, 9, 10]
+  },
+  jimbo: {
+    strokes:[ 5,12,9,22,13,7,16,10,11 ]
+  },
+  fish: {
+    strokes:[ 2,2,4,5,4,3,6,4,1]
+  },
+  course: {
+    strokes: [ 3,4,5,5,3,3,4,3,5 ]
+  }
+
+};
+var arrayOfPlayers = Object.keys(golfers);
+
+var coursePar = totalScores(golfers.course.strokes);
+var bobsTotal = totalScores(golfers.bob.strokes);
+var bobOverPar = bobsTotal - coursePar;
+
+var jimboTotal = totalScores(golfers.jimbo.strokes);
+var jimboOverPar = jimboTotal - coursePar;
+
+var fishTotal = totalScores(golfers.fish.strokes);
+
+
+
+var parCourse = ("Par for course is " + fishTotal + " strokes");
+var bob = ("Bob's total was " + bobsTotal + ". Bob went " + bobOverPar + " strokes over par and owes Fish $" + bobOverPar);
+var jimbo = ("Jimbo's total was " + jimboTotal + ". Jimbo went " + jimboOverPar +" strokes over par and owes Fish $" + jimboOverPar);
+var fish = ("Fish's total was " + fishTotal + " and won $" + (bobOverPar + jimboOverPar) + " on sidebets");
+document.querySelector('.bob').innerHTML = bob;
+document.querySelector('.jimbo').innerHTML = jimbo;
+document.querySelector('.fish').innerHTML = fish;
