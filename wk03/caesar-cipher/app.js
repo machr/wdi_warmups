80da94987aa96d26b0657158b2bbd10fd23402cb
Mark Powell <hi@powellmark.com>
2017-02-20 10:25:12 +1100

    well that didnt work

---
+++ wk03/caesar-cipher/app.js
@@ -0,0 +1,18 @@
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var shifted = "DEFGHIJKLMNOPQRSTUVWXYZABC";
+var message = "FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV, WKH YDOLDQW QHYHU WDVWH RI GHDWK EXW RQFH";
+// iucg
+alphabet = alphabet.split('');
+shifted = shifted.split('');
+message = message.split('');
+// find index of character from message in Alphabet - set that index to the index in shifted array
+// Return new character from shifted
+
+message.map(function(character){
+  indexOfChar = alphabet.indexOf(character);
+  var decoded;
+  for (var i = 0; i < message.length; i++) {
+    decoded = shifted[indexOfChar];
+  }
+  console.log(decoded);
+});
