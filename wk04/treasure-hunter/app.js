802ba1fc254948aacf9379b985da1a890e3044c9
Mark Powell <hi@powellmark.com>
2017-02-27 10:31:52 +1100

    Added conditional as per instruction

--- wk04/treasure-hunter/app.js
+++ wk04/treasure-hunter/app.js
@@ -12,22 +12,22 @@ var map2 = [
 ["A","A","A","A","A"],
 ["A","X","A","A","A"]
 ];
-//
-// var map3 = [
-// ["A","A","A","A","A"],
-// ["A","A","V","A","A"],
-// ["A","A","A","V","A"],
-// ["v","v","V","v","X"],
-// ["A","V","A","A","A"]
-// ];
-//
-// var map4 = [
-// ["A","A","A","A","A"],
-// ["A","A","V","A","A"],
-// ["A","A","A","V","A"],
-// ["v","v","V","v","A"],
-// ["X","V","A","A","A"]
-// ];
+
+var map3 = [
+["A","A","A","A","A"],
+["A","A","V","A","A"],
+["A","A","A","V","A"],
+["v","v","V","v","X"],
+["A","V","A","A","A"]
+];
+
+var map4 = [
+["A","A","A","A","A"],
+["A","A","V","A","A"],
+["A","A","A","V","A"],
+["v","v","V","v","A"],
+["X","V","A","A","A"]
+];
 
 var holyGrailMap = [
 ["v","v","V","v","S","S"],
@@ -41,10 +41,15 @@ var holyGrailMap = [
 ];
 
 function treasureFinder(array, key){
-  for (var i = 0; i < array.length; i++) {
-    if ( array[i].includes(key) ) {
-      var index = i + ", " + array[i].indexOf(key);
-      document.querySelector('#coordinate').textContent = index;
+  if (key === 'V') {
+    console.log("that doesn't belong in a museum");
+  } else {
+    for (var i = 0; i < array.length; i++) {
+      if ( array[i].includes(key) ) {
+        var index = i + ", " + array[i].indexOf(key);
+        document.querySelector('#coordinate').textContent = index;
+        // console.log(index);
+      }
     }
   }
 }
